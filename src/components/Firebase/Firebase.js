2be7d8531e84f944cb364618542b195d0ee06092
Matthew Ty <matthewty1220@gmail.com>
2021-12-02 05:16:22 -0700

    fixed firebase error

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,7 +1,8 @@
-import firebase from "firebase";
+import firebase from 'firebase/compat/app';
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCi7AtQeg7ypnGXsNEvchercoV7MiqCEec",
