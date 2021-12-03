a7a13b286ef8566dea867e3a5d6090fa2880a0a1
Matthew Ty <matthewty1220@gmail.com>
2021-12-02 20:27:06 -0700

    finally after troubleshooting, navbar is added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Profile from "../Profile/Profile";
+import Tracker from "../Tracker/Tracker";
+import WorkoutPlans from "../Workout PlansYT/WorkoutPlans";
+import WorkoutPlanner from "../Workout Planner/WorkoutPlanner";
 import Navbar from "../Navbar/Navbar";
 import "./Dashboard.css";
 
 function Dashboard() {
     return (
-        <div className="dashboard-container">
-        <Navbar />
-        </div>
+        
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/workoutplans' exact component={WorkoutPlans} />
+        <Route path='/workoutplanner' component={WorkoutPlanner} />
+        <Route path='/tracker' component={Tracker} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
+    </Router>
+        
     )
 };
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,19 +5,26 @@ function Navbar() {
 
 return (
     <nav>
-        <div>
-        <li>
-          <Link to="/workoutplans">Workouts</Link>
-        </li>
-        <li>
-          <Link to="/workoutplanner">Planner</Link>
-        </li>
-        <li>
-          <Link to="/tracker">Tracker</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
+        <div className="react-icon">
+               <Link style={{ textDecoration: 'none'}} to="/dashboard">Streak</Link>
+            <i class="fab fa-react" alt="Streak-Logo">
+            </i>
+        </div>
+        <div className="nav-list">
+            <ul className="ul-list-bar"id="ul-list">
+                <li>
+                    <Link style={{ textDecoration: 'none'}} to="/workoutplans">Workouts</Link>
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none'}} to="/workoutplanner">Planner</Link>
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none'}} to="/tracker">Tracker</Link>
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none'}} to="/profile">Profile</Link>
+                </li>
+            </ul>    
       </div>
     </nav>
         
