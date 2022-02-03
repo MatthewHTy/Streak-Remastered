e88e5a7adb884d0f33923d3a898affc53e932298
Matthew Ty <matthewty1220@gmail.com>
2022-02-03 16:08:04 -0700

    added streak title to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Welcome from '../Welcome/Welcome'
+import Welcome from "../Welcome/Welcome"
+import Home from "../Home/Home";
 // import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Profile from "../Profile/Profile";
@@ -10,30 +11,6 @@ import Navbar from "../Navbar/Navbar";
 import "./Dashboard.css";
 
 function Dashboard() {
-
-  // const [user, loading] = useAuthState(auth);
-  // const history = useHistory();
-
-  // const [userdata, setuserdata] = useState({
-  //   name: 'user',
-  //   email: 'email',
-  //   uid: '0',
-  //   tracker: 0,
-  //   planner: [],
-  //   likedWorkouts: [],
-  //   highscore: 0
-  // })
-
-  // const gituserdata = async () => {
-  //   setuserdata(await gituser(user)) 
-  // }
-
-  // useEffect(() => {
-  //   if (loading) return;
-  //   if (!user) return history.replace("/");
-  //   gituserdata();
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [user, loading]);
 
     return (
     <div>
@@ -47,7 +24,10 @@ function Dashboard() {
         <Route exact path='/tracker' component={Tracker} />
         <Route exact path='/profile' component={Profile} />
         {/* <Route path='/' component={SignOut} /> */}
-      <Welcome />
+        <div>
+          <Welcome />
+          <Home />
+        </div>
       </Switch>
     </Router>
       </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import "./Home.css"
+
+function Home() {
+
+    return(
+        <h1>Streak</h1>
+    );
+};
+
+export default Home;
